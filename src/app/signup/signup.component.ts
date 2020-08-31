a6d08e1d2e9bf18dac5d9976f9be585e02454098
Miljan <miljan.dujovicmd@gmail.com>
2020-09-01 01:02:37 +0200

    clock color - red green - works

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -29,5 +29,22 @@ export class SignupComponent implements OnInit {
 
  
 }
+/*MongoClient.connect("mongodb+srv://miljan123:miljan123@cluster0.9usha.mongodb.net/TimeTrackApp?retryWrites=true&w=majority", function(err, db) {
+  if (err) throw err;
+  
+  var dbo = db.db("TimeTrackApp");
+  var TimeTrackApp = [
+    { Id: 1, Email: 'admin', Password: 'admin'}, 
+    { Id: 2, Email: 'Desanka', Password: 'sifra'},
+    { Id: 3, Email: 'Nadezda', Password: 'sifra'},
+    { Id: 4, Email: 'Milunka', Password: 'sifra'},
+    
+  ];
+  dbo.collection("Users").insertMany(TimeTrackApp, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
 
-
+});
+*/
